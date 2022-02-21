a5c18e6b679fa06836ef2b4a880818db19dac83d
kremlevmax <kremlevmax1989@gmail.com>
2022-02-21 11:53:39 -0800

    Live stream circles added

--- js/script.js
+++ js/script.js
@@ -1,20 +1,19 @@
 function turnOnGallery() {
   document.getElementById("gallery__screen").style.backgroundImage =
     'url("/img/gallery/gallery.jpeg")';
-  document.getElementById("gallery__menu_gallery").innerHTML =
+  //   document.getElementById("gallery__menu_gallery").innerHTML =
 }
-    function turnOnStream1() {
-      document.getElementById("gallery__screen").style.backgroundImage =
-        'url("/img/gallery/stream_01.png")';
-    };
+function turnOnStream1() {
+  document.getElementById("gallery__screen").style.backgroundImage =
+    'url("/img/gallery/stream_01.png")';
+}
 
-  function turnOnStream2() {
-    document.getElementById("gallery__screen").style.backgroundImage =
-      'url("/img/gallery/stream_02.jpeg")';
-  }
+function turnOnStream2() {
+  document.getElementById("gallery__screen").style.backgroundImage =
+    'url("/img/gallery/stream_02.jpeg")';
+}
 
-  function turnOnStream3() {
-    document.getElementById("gallery__screen").style.backgroundImage =
-      'url("/img/gallery/stream_03.jpeg")';
-  }
-
+function turnOnStream3() {
+  document.getElementById("gallery__screen").style.backgroundImage =
+    'url("/img/gallery/stream_03.jpeg")';
+}
