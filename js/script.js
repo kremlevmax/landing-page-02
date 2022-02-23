fbf6b6e032c4deaaac8962a47924eda9bf80b76f
kremlevmax <kremlevmax1989@gmail.com>
2022-02-23 10:01:59 -0800

    Impact arts

--- js/script.js
+++ js/script.js
@@ -1,19 +1,27 @@
+let styleElem = document.head.appendChild(document.createElement("style"));
+let underline =
+  '::after {content: ""; position: absolute; bottom: -17px; left: 0; width: 100%; height: 4px; ; background-color: #fff}';
+
 function turnOnGallery() {
   document.getElementById("gallery__screen").style.backgroundImage =
     'url("/img/gallery/gallery.jpeg")';
-  //   document.getElementById("gallery__menu_gallery").innerHTML =
+  styleElem.innerHTML = "#gallery__menu_gallery" + underline;
 }
+
 function turnOnStream1() {
   document.getElementById("gallery__screen").style.backgroundImage =
     'url("/img/gallery/stream_01.png")';
+  styleElem.innerHTML = "#gallery__menu_stream-1" + underline;
 }
 
 function turnOnStream2() {
   document.getElementById("gallery__screen").style.backgroundImage =
     'url("/img/gallery/stream_02.jpeg")';
+  styleElem.innerHTML = "#gallery__menu_stream-2" + underline;
 }
 
 function turnOnStream3() {
   document.getElementById("gallery__screen").style.backgroundImage =
     'url("/img/gallery/stream_03.jpeg")';
+  styleElem.innerHTML = "#gallery__menu_stream-3" + underline;
 }
