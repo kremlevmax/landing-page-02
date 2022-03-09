84b3e54b2da9fa9b76ece87ead62ef7ca5a941e9
kremlevmax <kremlevmax1989@gmail.com>
2022-03-09 00:46:43 -0800

    Underline bug fix

--- js/script.js
+++ js/script.js
@@ -1,27 +1,31 @@
 let styleElem = document.head.appendChild(document.createElement("style"));
 let underline =
-  '::after {content: ""; position: absolute; bottom: -17px; left: 0; width: 100%; height: 4px; ; background-color: #fff}';
+  '::after {content: ""; position: absolute;  left: 0; bottom: -17px; width: 100%; height: 4px; ; background-color: #fff}';
 
 function turnOnGallery() {
   document.getElementById("gallery__screen").style.backgroundImage =
     'url("/img/gallery/gallery.jpeg")';
-  styleElem.innerHTML = "#gallery__menu_gallery" + underline;
+  styleElem.innerHTML =
+    '#gallery__menu_gallery::after {content: ""; position: absolute;  left: 0; bottom: -17px; width: 100%; height: 4px; ; background-color: #fff}';
 }
 
 function turnOnStream1() {
   document.getElementById("gallery__screen").style.backgroundImage =
     'url("/img/gallery/stream_01.png")';
-  styleElem.innerHTML = "#gallery__menu_stream-1" + underline;
+  styleElem.innerHTML =
+    '#gallery__menu_stream-1::after {content: ""; position: absolute;  left: 0; bottom: -17px; width: 100%; height: 4px; ; background-color: #fff}';
 }
 
 function turnOnStream2() {
   document.getElementById("gallery__screen").style.backgroundImage =
     'url("/img/gallery/stream_02.jpeg")';
-  styleElem.innerHTML = "#gallery__menu_stream-2" + underline;
+  styleElem.innerHTML =
+    '#gallery__menu_stream-2::after {content: ""; position: absolute;  left: 0; bottom: -17px; width: 100%; height: 4px; ; background-color: #fff}';
 }
 
 function turnOnStream3() {
   document.getElementById("gallery__screen").style.backgroundImage =
     'url("/img/gallery/stream_03.jpeg")';
-  styleElem.innerHTML = "#gallery__menu_stream-3" + underline;
+  styleElem.innerHTML =
+    '#gallery__menu_stream-3::after {content: ""; position: absolute;  left: 0; bottom: -17px; width: 100%; height: 4px; ; background-color: #fff}';
 }
