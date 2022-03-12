11984dced783213a01fc4be77b201ff44f28e1f4
kremlevmax <kremlevmax1989@gmail.com>
2022-03-12 13:10:13 -0800

    First screen fix

--- js/script.js
+++ js/script.js
@@ -4,28 +4,28 @@ let underline =
 
 function turnOnGallery() {
   document.getElementById("gallery__screen").style.backgroundImage =
-    'url("/img/gallery/gallery.jpeg")';
+    'url("img/gallery/gallery.jpeg")';
   styleElem.innerHTML =
     '#gallery__menu_gallery::after {content: ""; position: absolute;  left: 0; bottom: -17px; width: 100%; height: 4px; ; background-color: #fff}';
 }
 
 function turnOnStream1() {
   document.getElementById("gallery__screen").style.backgroundImage =
-    'url("/img/gallery/stream_01.png")';
+    'url("img/gallery/stream_01.png")';
   styleElem.innerHTML =
     '#gallery__menu_stream-1::after {content: ""; position: absolute;  left: 0; bottom: -17px; width: 100%; height: 4px; ; background-color: #fff}';
 }
 
 function turnOnStream2() {
   document.getElementById("gallery__screen").style.backgroundImage =
-    'url("/img/gallery/stream_02.jpeg")';
+    'url("img/gallery/stream_02.jpeg")';
   styleElem.innerHTML =
     '#gallery__menu_stream-2::after {content: ""; position: absolute;  left: 0; bottom: -17px; width: 100%; height: 4px; ; background-color: #fff}';
 }
 
 function turnOnStream3() {
   document.getElementById("gallery__screen").style.backgroundImage =
-    'url("/img/gallery/stream_03.jpeg")';
+    'url("img/gallery/stream_03.jpeg")';
   styleElem.innerHTML =
     '#gallery__menu_stream-3::after {content: ""; position: absolute;  left: 0; bottom: -17px; width: 100%; height: 4px; ; background-color: #fff}';
 }
