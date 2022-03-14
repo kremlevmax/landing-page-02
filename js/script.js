a53e9aa1dd715be80a110956c2eb4d8f641c8221
kremlevmax <kremlevmax1989@gmail.com>
2022-03-13 23:33:01 -0700

    Some JS Fix

--- js/script.js
+++ js/script.js
@@ -2,30 +2,32 @@ let styleElem = document.head.appendChild(document.createElement("style"));
 let underline =
   '::after {content: ""; position: absolute;  left: 0; bottom: -17px; width: 100%; height: 4px; ; background-color: #fff}';
 
-function turnOnGallery() {
+const turnOnGallery = () => {
   document.getElementById("gallery__screen").style.backgroundImage =
     'url("img/gallery/gallery.jpeg")';
   styleElem.innerHTML =
     '#gallery__menu_gallery::after {content: ""; position: absolute;  left: 0; bottom: -17px; width: 100%; height: 4px; ; background-color: #fff}';
-}
+};
 
-function turnOnStream1() {
+const turnOnStream1 = () => {
   document.getElementById("gallery__screen").style.backgroundImage =
     'url("img/gallery/stream_01.png")';
   styleElem.innerHTML =
     '#gallery__menu_stream-1::after {content: ""; position: absolute;  left: 0; bottom: -17px; width: 100%; height: 4px; ; background-color: #fff}';
-}
+};
 
-function turnOnStream2() {
+const turnOnStream2 = () => {
   document.getElementById("gallery__screen").style.backgroundImage =
     'url("img/gallery/stream_02.jpeg")';
   styleElem.innerHTML =
     '#gallery__menu_stream-2::after {content: ""; position: absolute;  left: 0; bottom: -17px; width: 100%; height: 4px; ; background-color: #fff}';
-}
+};
 
-function turnOnStream3() {
+const turnOnStream3 = () => {
   document.getElementById("gallery__screen").style.backgroundImage =
     'url("img/gallery/stream_03.jpeg")';
   styleElem.innerHTML =
     '#gallery__menu_stream-3::after {content: ""; position: absolute;  left: 0; bottom: -17px; width: 100%; height: 4px; ; background-color: #fff}';
-}
+};
+
+const changeNumber = () => {};
