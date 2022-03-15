38b34521de22361a62ddf40037416e52b8698309
kremlevmax <kremlevmax1989@gmail.com>
2022-03-14 20:14:57 -0700

    Number counter JS

--- js/script.js
+++ js/script.js
@@ -30,4 +30,46 @@ const turnOnStream3 = () => {
     '#gallery__menu_stream-3::after {content: ""; position: absolute;  left: 0; bottom: -17px; width: 100%; height: 4px; ; background-color: #fff}';
 };
 
-const changeNumber = () => {};
+// number change
+
+// const callback = (entries) => {
+//   entries.forEach((entry) => {
+//     if (entry.isIntersecting) {
+//       entry.target.classList.add("element-show");
+//     }
+//   });
+// };
+
+// let options = {
+//   threshold: 1.0,
+// };
+
+// let observer = new IntersectionObserver(callback, options);
+// let elements = document.querySelectorAll("appointment__text-main-number");
+// for (let element in elements) {
+//   console.log(elements);
+//   // observer.observe(element);
+// }
+
+const target = document.querySelector(".appointment__text-main-number");
+let i = 0;
+
+function handleIntersection(entries) {
+  entries.map((entry) => {
+    if (entry.isIntersecting) {
+      const changeNumber = () => {
+        entry.target.innerText = i;
+        i++;
+        if (i === 5700) {
+          clearInterval(interval);
+        }
+      };
+
+      let interval = setInterval(changeNumber, 1);
+    }
+  });
+}
+
+const observer = new IntersectionObserver(handleIntersection);
+
+observer.observe(target);
